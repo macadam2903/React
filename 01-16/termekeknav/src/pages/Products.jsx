b3b7da9d85c5b1f2fe4617a14fccf7ae5962df68
macadam2903 <macadammaci@gmail.com>
2026-01-16 10:13:05 +0100

    css added

--- 01-16/termekeknav/src/pages/Products.jsx
+++ 01-16/termekeknav/src/pages/Products.jsx
@@ -1,6 +1,9 @@
 const Products = () => {
-    return <h1>Products Page</h1>;
-
+    return (
+        <div>
+            <h1>Kosar Page</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis inventore recusandae tempora incidunt veritatis quae nisi, eum voluptates, eligendi laborum sit cum magnam porro culpa, ipsam unde impedit et at.</p>
+        </div>
+    );
     }
-    
 export default Products;
